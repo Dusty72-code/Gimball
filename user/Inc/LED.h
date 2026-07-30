22e6073553f68ae3164da4b9b3545c61475f062e
Dusty <2908251885@qq.com>
2026-07-30 19:22:02 +0800

    LED

---
+++ user/Inc/LED.h
@@ -0,0 +1,15 @@
+//
+// Created by Dolores on 2026/7/30.
+//
+
+#ifndef GIMBALL_LED_H
+#define GIMBALL_LED_H
+
+#include "main.h"
+
+#define LED_BREATH_STEP_MS 10U
+#define LED_PWM_RESOLUTION 100U
+
+void LED_Status_Init(void);
+
+#endif //GIMBALL_LED_H
