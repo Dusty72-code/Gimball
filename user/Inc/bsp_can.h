e9f511141d639e5de21d5c0d41d8ce01562aad96
Dusty <2908251885@qq.com>
2026-07-30 17:58:44 +0800

    减小FreeRTOS堆大小

---
+++ user/Inc/bsp_can.h
@@ -0,0 +1,8 @@
+//
+// Created by Dolores on 2026/7/30.
+//
+
+#ifndef GIMBALL_BSP_CAN_H
+#define GIMBALL_BSP_CAN_H
+
+#endif //GIMBALL_BSP_CAN_H
---
+++ user/Src/bsp_can.c
@@ -0,0 +1,3 @@
+//
+// Created by Dolores on 2026/7/30.
+//
