bcf84e7397663dacd4598fb01d7019266f29e597
Dusty <2908251885@qq.com>
2026-08-05 21:27:39 +0800

    main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,7 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "system_task_init.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -95,7 +95,7 @@ int main(void)
   MX_CAN_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-
+  System_Periph_Init();
   /* USER CODE END 2 */
 
   /* Init scheduler */
